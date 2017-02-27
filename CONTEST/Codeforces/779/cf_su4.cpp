a701e093f74a887784ac2c0b51b612f6fcd99b9c
Ramesh Chandra <rameshc10695@gmail.com>
2017-02-27 10:46:05 +0530

    D Problem CodeForces...First Tym
    Problem Ref :
    http://codeforces.com/contest/779/problem/D

--- CONTEST/Codeforces/779/cf_su4.cpp
+++ CONTEST/Codeforces/779/cf_su4.cpp
@@ -1,30 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define mp make_pair
-bool __cmp(pair<pair<int,int> ,int >A,pair< pair<int,int>,int  > B){
-    if(A.first.first-A.first.second != B.first.first-B.first.second)
-        return A.first.first-A.first.second < B.first.first-B.first.second;
-    return A.first.first<B.first.first;
+string text,pattern;
+int eraser[200005];
+bool mark_erased[200005];
+bool check(int pos,int N){
+    for(int i=0;i<N;i++) //flush old value from mark_erased
+        mark_erased[i]=false;
+    for(int i=0;i<pos;i++) //marking erased element till pos
+        mark_erased[eraser[i]]=true;
+    for(int i=0,j=0;i<N;i++){
+        if(!mark_erased[i]){ //this element is not erased
+                             //we need to find matching for this...
+            if(pattern[j]==text[i]){
+                j++; //move to next character for matching..
+                if(j>=pattern.length()) //completely matched
+                    return true;
+            }else ; //if it not matched
+
+                    //we will check with next unerased character
+        }
+    }
+    return false;
 }
 int main(){
-    freopen("in.txt","r",stdin);
-    string a,b;
-    cin >> a >> b;
-    int n = a.length();
-    int idx[n];
-    for(int i=0;i<n;i++)
-        cin >>idx[i];
-    map<string,bool> mp;
-    mp[b]=true;
-    int ans = 0;
-    for(int i=0;i<n;i++){
-        if(mp.find(a)!=mp.end())
-            break;
-        ans++;
-        a.erase(a.begin()+idx[i]);
-        cout<<a<<endl;
+    cin>>text;
+    cin >>pattern;
+    int N=text.length();
+    for(int i=0;i<N;i++)
+      cin>>eraser[i],--eraser[i];
+    int low=0,high=N-1,ans=0;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(check(mid,N))
+            ans = mid,low=mid+1;
+        else
+            high = mid-1;
     }
     cout<<ans<<endl;
     return 0;
 }
-
